8f38f9c4826df28c4337f12feee427a827d211d0
hawkjay7781 <45582159+hawkjay7781@users.noreply.github.com>
2018-12-23 15:26:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+	document.getElementById("sideNavStyle").style.width = "250px";
+	
+}
+
+function closeNav() {
+	document.getElementById("sideNavStyle").style.width = "0";
+	
+}
